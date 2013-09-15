4e14c9f3f643cc00333ab03099a5d800b62a575c
Simon Schraeder <simon.schraeder@yahoo.de>
2013-09-15 22:59:35 +0200

    v1.0.1 - Improved Cover
    Improved cover by adding API for deezer, that get's called in case
    last.fm has no cover. Not 100% accurate, but better than displaying no
    cover at all.

--- data.php
+++ data.php
@@ -8,7 +8,7 @@ $scrobbler_url = "http://ws.audioscrobbler.com/2.0/user/" . $username . "/recent
 
 //let's load the data first
 if ($scrobbler_xml = file_get_contents($scrobbler_url)) {
-    
+   
     //Start parsing the data
     $scrobbler_data = simplexml_load_string($scrobbler_xml);
     //Select the latest track
@@ -27,16 +27,24 @@ if ($scrobbler_xml = file_get_contents($scrobbler_url)) {
     //echo the artist and track name  
     $data["artist"] = $track->artist;
     $data["title"] = $track->name;  
-    $data["cover"] = $track->image[3];
+    //$data["cover"] = $track->image[3];
+        
     
     //Search for the track on spotify    
-    $track = Spotify::searchTrack($track->name . ' - ' . $track->artist);
+    $strack = Spotify::searchTrack($track->name . ' - ' . $track->artist);
     
     // Receive the Spotify URI for the first track
     // Please note, that this is not 100% accurate
-    $uri = Spotify::getUri($track);
+    $uri = Spotify::getUri($strack);
     $data["spotify"] = $uri;
-    
+        
+    //If last.fm has no cover, ask deezer    
+	if(strpos($data["cover"], 'http://') === false) {
+    	$deezer = file_get_contents("http://api.deezer.com/search?q=".urlencode($track->artist." - ".$track->album));
+	  	$cover = json_decode($deezer);
+		$data["cover"] = $cover->data[0]->album->cover;
+	}
+
     echo json_encode($data);
     
  }
